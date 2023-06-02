64a717b329142b550fb04baa90c1704b23999737
Lucas Lyck <lycklucas@gmail.com>
2023-06-02 12:59:07 +0200

    TEST

---
+++ Exercises/1. Imageclassification/Exercise_1_4.py
@@ -0,0 +1,12 @@
+import numpy as np
+from tqdm.notebook import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+print("hello")
