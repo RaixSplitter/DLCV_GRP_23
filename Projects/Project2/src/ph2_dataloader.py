57a4e8e70a394e1421608efe033bb6ca34557153
Pere Ginebra <pere.ginebra@gmail.com>
2023-06-09 13:40:09 +0200

    added get_dataloader

--- Projects/Project2/src/ph2_dataloader.py
+++ Projects/Project2/src/ph2_dataloader.py
@@ -2,7 +2,7 @@ import torch
 import os
 import numpy as np
 from torchvision import datasets, transforms
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 from PIL import Image
 
 class ph2_dataset(Dataset):
@@ -25,3 +25,16 @@ class ph2_dataset(Dataset):
         X = self.transform(image)
         Y = self.transform(label)
         return X, Y
+    
+def get_dataloaders():
+    data = ph2_dataset()
+    train_size = int(0.75*len(data))
+    test_size = int(0.15*len(data))
+    val_size = len(data) - train_size - test_size
+
+    train, test, val = random_split(data, [train_size, test_size, val_size])
+    train_dl = DataLoader(train, batch_size=32, shuffle=True)
+    test_dl = DataLoader(test, batch_size=32, shuffle=False)
+    val_dl = DataLoader(val, batch_size=32, shuffle=False)
+    
+    return train_dl, test_dl, val_dl
