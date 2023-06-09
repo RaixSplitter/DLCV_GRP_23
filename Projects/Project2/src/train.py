0a3e9edf2536ee6dfab5743da27a90c71ad91327
Lucas Lyck <lycklucas@gmail.com>
2023-06-09 13:22:19 +0200

    added training loop

--- Projects/Project2/src/train.py
+++ Projects/Project2/src/train.py
@@ -13,11 +13,16 @@ from torchvision import models
 from torchsummary import summary
 import torch.optim as optim
 from time import time
+import sys
+import os
 
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
+#Our imports
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from data_utils import DRIVEDataset, get_data_loader
+from model.baseline import EncDec
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
@@ -28,6 +33,7 @@ image_path = os.path.join(current_path, '..', 'data', 'DRIVE', 'training', 'imag
 vessel_mask = os.path.join(current_path, '..', 'data', 'DRIVE', 'training', '1st_manual')
 eye_mask = os.path.join(current_path, '..', 'data', 'DRIVE', 'training', 'mask')
 plot_path = os.path.join(current_path, '..', 'plots')
+results_path = os.path.join(current_path, '..', 'results')
 
 size = 128 # Image size 
 train_dataset, val_dataset, test_dataset, train_dataloader, val_dataloader, test_dataloader = get_data_loader(image_path, vessel_mask, size)
@@ -36,12 +42,17 @@ print('Loaded %d training images' % len(train_dataset))
 print('Loaded %d val images' % len(val_dataset))
 print('Loaded %d test images' % len(test_dataset))
 
+def bce_loss(y_real, y_pred):
+    return torch.mean(y_pred - y_real*y_pred + torch.log(1 + torch.exp(-y_pred)))
 
-def train(model, opt, loss_fn, epochs, train_loader, test_loader):
+learning_rate = 0.001
+epochs = 20
+
+def train(model, opt, loss_fn, epochs, train_loader, test_loader, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
     X_test, Y_test = next(iter(test_loader))
 
     for epoch in range(epochs):
-        tic = time()
         print('* Epoch %d/%d' % (epoch+1, epochs))
 
         avg_loss = 0
@@ -61,22 +72,28 @@ def train(model, opt, loss_fn, epochs, train_loader, test_loader):
 
             # calculate metrics to show the user
             avg_loss += loss / len(train_loader)
-        toc = time()
         print(' - loss: %f' % avg_loss)
 
         # show intermediate results
         model.eval()  # testing mode
-        Y_hat = F.sigmoid(model(X_test.to(device))).detach().cpu()
+        Y_hat = torch.sigmoid(model(X_test.to(device))).detach().cpu()
         clear_output(wait=True)
-        for k in range(6):
-            plt.subplot(2, 6, k+1)
-            plt.imshow(np.rollaxis(X_test[k].numpy(), 0, 3), cmap='gray')
+        for k in range(2):  # change this to change the number of images shown
+            plt.subplot(2, 2, k+1)
+            plt.imshow(np.transpose(X_test[k].numpy(), (1, 2, 0)), cmap='gray')
             plt.title('Real')
             plt.axis('off')
 
-            plt.subplot(2, 6, k+7)
+            plt.subplot(2, 2, k+3)
             plt.imshow(Y_hat[k, 0], cmap='gray')
             plt.title('Output')
             plt.axis('off')
+        
         plt.suptitle('%d / %d - loss: %f' % (epoch+1, epochs, avg_loss))
-        plt.show()
+        plt.savefig(os.path.join(save_dir, f"epoch_{epoch+1}_results.png"))  # Save the figure
+        plt.clf()  # Clear the current figure for the next plot
+
+
+model = EncDec().to(device)
+
+train(model, optim.Adam(model.parameters(), lr=learning_rate), bce_loss, epochs, train_dataloader, val_dataloader, results_path)
