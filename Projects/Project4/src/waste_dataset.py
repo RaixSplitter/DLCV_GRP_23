4f89e502fa2c8ae37704721e5489ee9cb7892418
Lucas Lyck <lycklucas@gmail.com>
2023-06-16 16:41:33 +0200

    added dataloader for selevtice search

--- Projects/Project4/src/waste_dataset.py
+++ Projects/Project4/src/waste_dataset.py
@@ -34,25 +34,40 @@ class WasteDataset(Dataset):
         return self.transform(subimage), label
 
 class WasteDatasetImages(Dataset):
-    def __init__(self, transform=None):
+    def __init__(self, transform=None, resize=None):
         with open(os.path.join(DATA_PATH, 'annotations.json')) as f:
             data = json.load(f)
         self.transform = transform if transform is not None else transforms.Compose([
-            transforms.Resize((228, 228)),
             transforms.ToTensor()
         ])
         self.img_info = data['images']
         self.annotation = data['annotations']
+        self.resize = resize  # Specify the desired resize dimensions
     
     def __len__(self):
-        return len(self.annotation)
+        return len(self.img_info)
     
     def __getitem__(self, idx):
-        item = self.annotation[idx]
-        src_img_file = self.img_info[item['image_id']]['file_name']
+        img_info = self.img_info[idx]
+        img_id = img_info['id']
+        src_img_file = img_info['file_name']
         src_img = Image.open(os.path.join(DATA_PATH, src_img_file))
+        
+        # Resize the image
+        resized_img = src_img.resize(self.resize)
+        transformed_img = self.transform(resized_img)
+        
+        bboxes = [ann['bbox'] for ann in self.annotation if ann['image_id'] == img_id]
+        #print(f"Found {len(bboxes)} bounding boxes for image {img_id}")
+        resized_bboxes = []
+        for bbox in bboxes:
+            resized_bbox = [
+                bbox[0] * self.resize[0] / src_img.width,  # x
+                bbox[1] * self.resize[1] / src_img.height,  # y
+                bbox[2] * self.resize[0] / src_img.width,  # width
+                bbox[3] * self.resize[1] / src_img.height  # height
+            ]
+            resized_bboxes.append(resized_bbox)
 
-        src_img = self.transform(src_img)
-
-        return src_img
+        return transformed_img, resized_bboxes
 
